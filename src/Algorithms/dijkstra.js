5d1a706b14649298b5aaf4201520ab628c92795c
aembar <52722485+aembar@users.noreply.github.com>
2021-09-14 14:37:42 -0700

    Update dijkstra.js

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -1,66 +1,57 @@
-// Performs Dijkstra's algorithm; returns *all* nodes in the order
-// in which they were visited. Also makes nodes point back to their
-// previous node, effectively allowing us to compute the shortest path
-// by backtracking from the finish node.
-export function dijkstra(grid, startNode, finishNode) {
-  const visitedNodesInOrder = [];
-  startNode.distance = 0;
-  const unvisitedNodes = getAllNodes(grid);
-  while (!!unvisitedNodes.length) {
-    sortNodesByDistance(unvisitedNodes);
-    const closestNode = unvisitedNodes.shift();
-    // If we encounter a wall, we skip it.
-    if (closestNode.isWall) continue;
-    // If the closest node is at a distance of infinity,
-    // we must be trapped and should therefore stop.
-    if (closestNode.distance === Infinity) return visitedNodesInOrder;
-    closestNode.isVisited = true;
-    visitedNodesInOrder.push(closestNode);
-    if (closestNode === finishNode) return visitedNodesInOrder;
-    updateUnvisitedNeighbors(closestNode, grid);
+export function dijkstra(area, beg, end) {
+  const order = [];
+  beg.distance = 0;
+  const unvisited = getNodes(area);
+  while (unvisited.length) {
+    sortDist(unvisited);
+    const close = unvisited.shift();
+    if (close.distance === Infinity){ return order;}
+    if (close.isWall) {continue;}
+    order.push(close);
+    close.isVisited = true;
+    if (close === end) {return order;}
+    updateUnvisited(close, area);
   }
 }
 
-function sortNodesByDistance(unvisitedNodes) {
-  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
-}
-
-function updateUnvisitedNeighbors(node, grid) {
-  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
-  for (const neighbor of unvisitedNeighbors) {
-    neighbor.distance = node.distance + 1;
-    neighbor.previousNode = node;
-  }
-}
-
-function getUnvisitedNeighbors(node, grid) {
-  const neighbors = [];
-  const {col, row} = node;
-  if (row > 0) neighbors.push(grid[row - 1][col]);
-  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
-  if (col > 0) neighbors.push(grid[row][col - 1]);
-  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
-  return neighbors.filter(neighbor => !neighbor.isVisited);
-}
-
-function getAllNodes(grid) {
+function getUnvisited(val, area) {
   const nodes = [];
-  for (const row of grid) {
-    for (const node of row) {
-      nodes.push(node);
+  const {column, row} = val;
+  if (column > 0) {nodes.push(area[row][column - 1]);}
+  if (row > 0) {nodes.push(area[row - 1][column]);}
+  if (column < area[0].length - 1){ nodes.push(area[row][column + 1]);}
+  if (row < area.length - 1) {nodes.push(area[row + 1][column]);}
+  return nodes.filter(neighbor => !neighbor.isVisited);
+}
+
+function getNodes(area) {
+  const values = [];
+  for (const rows of area) {
+    for (const value of rows) {
+      values.push(value);
     }
   }
-  return nodes;
+  return values;
 }
 
-// Backtracks from the finishNode to find the shortest path.
-// Only works when called *after* the dijkstra method above.
-export function getNodesInShortestPathOrder(finishNode) {
-  const nodesInShortestPathOrder = [];
-  let currentNode = finishNode;
-  while (currentNode !== null) {
-    nodesInShortestPathOrder.unshift(currentNode);
-    currentNode = currentNode.previousNode;
+function sortDist(unvisited) {
+  unvisited.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
+
+function updateUnvisited(node, area) {
+  const unvisited = getUnvisited(node, area);
+  for (const nodes of unvisited) {
+    nodes.previousNode = node;
+    nodes.distance = node.distance + 1;
   }
-  return nodesInShortestPathOrder;
+}
+
+export function getNodesShortestPath(endNode) {
+  const shortNode = [];
+  let curr = endNode;
+  while (curr) {
+    curr = curr.previousNode;
+    shortNode.unshift(curr);
+  }
+  return shortNode;
 }
